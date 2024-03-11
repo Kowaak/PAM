c61cf302cd8c078f30f3c6d72d6fb2c1b62e97e1
Kowaak <zalecki.filip@gmail.com>
2024-03-11 15:21:34 +0100

    a

---
+++ AndroidApp2/AndroidApp2/MainActivity.cs
@@ -0,0 +1,47 @@
+namespace AndroidApp2
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            EditText editText1 = FindViewById<EditText>(Resource.Id.editText1);
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            TextView textView1 = FindViewById<TextView>(Resource.Id.textView1);
+
+            long Factorial(int n)
+            {
+                if (n == 0) return 1;
+                return n * Factorial(n - 1);
+            }
+
+            button1.Click += delegate
+            {
+                if (string.IsNullOrEmpty(editText1.Text))
+                {
+                    textView1.Text = "WprowadŸ liczbê";
+                    return;
+                }
+
+                if (!int.TryParse(editText1.Text, out int inputValue))
+                {
+                    textView1.Text = "WprowadŸ liczbê ca³kowit¹";
+                    return;
+                }
+
+                try
+                {
+                    long factorial = Factorial(inputValue);
+                    textView1.Text = "Silnia: " + factorial.ToString();
+                }
+                catch (ArgumentException e)
+                {
+                    textView1.Text = e.Message;
+                }
+            };
+        }
+    }
+}
---
+++ AndroidApp2/AndroidApp2/obj/Debug/net8.0-android/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,10 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "AndroidApp2.dll" comes first in this list. */
+		"AndroidApp2.dll",
+		"_Microsoft.Android.Resource.Designer.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+}
