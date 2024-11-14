38196b4f84cb426a5c6265d333e1f8f3afe255c9
Kowaak <102755037+Kowaak@users.noreply.github.com>
2024-11-14 10:50:59 +0100

    Komentarze

--- Hasla/app/src/main/java/com/example/hasla/RegisterActivity.java
+++ Hasla/app/src/main/java/com/example/hasla/RegisterActivity.java
@@ -20,7 +20,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -28,7 +27,8 @@ public class RegisterActivity extends AppCompatActivity {
 
         EditText Elogin = findViewById(R.id.eLogin);
         EditText Ehaslo = findViewById(R.id.eHaslo);
-        //Utworzenie lub otwarcie bazy danych z nazwą określona w zmiennej 'DB_NAME' dziedziczoną z klasy 'Global'
+        TextView Error = findViewById(R.id.log_error);
+        //Utworzenie lub otwarcie bazy danych z nazwą określona w zmiennej 'DB_NAME' z klasy 'Global'
         SQLiteDatabase db = openOrCreateDatabase(DB_NAME, Context.MODE_PRIVATE, null);
         db.execSQL("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,Login TEXT UNIQUE, Haslo TEXT )");
 
@@ -44,28 +44,28 @@ public class RegisterActivity extends AppCompatActivity {
             //Cała funkcja przycisku z id 'Rejestracja'
             Button rejeButton = findViewById(R.id.Rejestracja);
             rejeButton.setOnClickListener(view ->{
-                TextView Error = findViewById(R.id.log_error);
                 try {
                     String Login = Elogin.getText().toString();
                     String Haslo = Ehaslo.getText().toString();
                     //Walidacja czy pola sa puste
                     if (Login.isEmpty() || Haslo.isEmpty()) {
                         Error.setText(R.string.error_empty);
-                        return; //Jeśli są puste to wyjdź
+                        return; //Jeśli jakieś pole jest puste to wyjdź i pokaż błąd
                     }
                     //Walidacja czy login jest unikatowy
                     Cursor cursor = db.rawQuery("SELECT * FROM users WHERE Login = ?", new String[]{Login});
                     if (cursor.getCount() > 0) {
-                        //Login już istnieje
+                        //Login już istnieje, pokaż błąd
                         Error.setText(R.string.error_login_taken);
                     } else {
-                        //Insert użytkownika do bazy
+                        //Insert użytkownika do bazy i pokazanie komunikatu jeśli się udało
                         db.execSQL("INSERT INTO users (Login, Haslo) VALUES (?, ?)", new String[]{Login, Haslo});
-                        //Udało się utworzyć użytkownika
                         Toast.makeText(RegisterActivity.this, "UTWORZONO UŻYTKOWNIKA", Toast.LENGTH_SHORT).show();
                     }
+                    //Zamknięcie połaczenia z bazą
                     cursor.close();
                 } catch (Exception e) {
+                    //Jeśli coś pójdzie nie tak to wyświetl bład
                     Error.setText("Błąd: " + e.getMessage());
                 }
             });
