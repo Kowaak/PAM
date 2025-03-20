bcf6d5a393cbd0cc3e6077640d7f60c0f708c2bd
Kowaak <>
2025-03-20 10:28:25 +0100

    WTF

--- kosci/app/src/main/java/com/example/kosci/MainActivity.java
+++ kosci/app/src/main/java/com/example/kosci/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.kosci;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +27,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
             Random rand = new Random();
+            int[] tab = new int[6];
+            Button losuj = findViewById(R.id.losuj);
 
-            int rand_int1 = rand.nextInt(5);
-            int rand_int2 = rand.nextInt(5);
-            int rand_int3 = rand.nextInt(5);
-            int rand_int4 = rand.nextInt(5);
-            int rand_int5 = rand.nextInt(5);
-            int rand_int6 = rand.nextInt(5);
-            
+            ImageView ko1 = findViewById(R.id.k1);
+            ImageView ko2 = findViewById(R.id.k2);
+            ImageView ko3 = findViewById(R.id.k3);
+            ImageView ko4 = findViewById(R.id.k4);
+            ImageView ko5 = findViewById(R.id.k5);
+            ImageView ko6 = findViewById(R.id.k6);
+            TextView wynik = findViewById(R.id.wynik);
+
+            losuj.setOnClickListener(v1 -> {
+                int suma = 0;
+                int[] drawables = {R.drawable.k1, R.drawable.k2, R.drawable.k3, R.drawable.k4, R.drawable.k5, R.drawable.k6};
+                for (int i = 0; i < 6; i++) {
+                    tab[i] = rand.nextInt(6);
+                    tab[i] = tab[i] + 1;
+                    suma += tab[i];
+                }
+
+                ko1.setImageResource(drawables[tab[0] - 1]);
+                ko2.setImageResource(drawables[tab[1] - 1]);
+                ko3.setImageResource(drawables[tab[2] - 1]);
+                ko4.setImageResource(drawables[tab[3] - 1]);
+                ko5.setImageResource(drawables[tab[4] - 1]);
+                ko6.setImageResource(drawables[tab[5] - 1]);
+                wynik.setText("Wynik losowania: " + suma);
+            });
+            //https://pl.wikipedia.org/wiki/Ko%C5%9Bci_(gra)
             return insets;
         });
     }
